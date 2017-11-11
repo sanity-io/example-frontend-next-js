28197c162bc2c8403e2d1d70d9afbb29f15890f9
Bjørge Næss <bjoerge@gmail.com>
2017-11-11 13:08:12 +0100

    Hearts and logos

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,22 @@
 import Menu from './Menu'
+import Head from 'next/head'
 
 export default props => (
   <div>
+    <Head>
+      <meta charset="utf-8" />
+      <title>Sanity + Next.js = 💖</title>
+    </Head>
     <Menu />
     <div>
       {props.children}
+    </div>
+    <div>
+      <img src="https://rawgit.com/sanity-io/example-frontend-next-js/master/static/sanity-logo.svg"/>
+      +
+      <img src="https://rawgit.com/sanity-io/example-frontend-next-js/master/static/nextjs-logo.svg"/>
+      =
+      💖
     </div>
   </div>
 )
