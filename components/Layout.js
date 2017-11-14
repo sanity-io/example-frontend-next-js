d66d1e3c75ea12ca3875f00a315e6e762cdcf5b6
Bjørge Næss <bjoerge@gmail.com>
2017-11-14 12:53:18 +0100

    Set viewport

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ export default props => (
   <div>
     <Head>
       <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Sanity + Next.js = 💖</title>
     </Head>
     <nav>
