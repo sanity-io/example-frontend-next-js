ff1d7e9ba7bbe18b7f0a14ce219256be1c52d352
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-04-19 09:21:11 +0200

    Adding block content

--- pages/movie.js
+++ pages/movie.js
@@ -24,6 +24,39 @@ const query = `*[_type == "movie" && _id == $id] {
 }[0]
 `
 
+const serializers = {
+  types: {
+    summaries: props => {
+      const {node} = props
+      if (!node) {
+        return false
+      }
+      const {summaries} = node
+      if (!summaries || summaries.length === 0) {
+        return false
+      }
+      return (
+        <div className="summaries">
+          <h2>{node.caption}</h2>
+          <ul>
+            {
+              summaries.map(summary => {
+                return (
+                  <li key={summary._key}>
+                    <BlockContent blocks={summary.summary} serializers={serializers} />
+                    — <a href={summary.url}>{summary.author}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <style jsx>{styles}</style>
+        </div>
+      )
+    }
+  }
+}
+
 export default class Movie extends React.Component {
 
   static async getInitialProps(req) {
@@ -34,18 +67,22 @@ export default class Movie extends React.Component {
 
   render() {
     const {movie} = this.props
-    console.log('movie', movie)
     return (
       <Layout>
         <div className="movie">
-          <div className="movie__header">
+          <div className="header">
             {movie.posterUrl && <img src={`${movie.posterUrl}?h=480`} />}
             {movie.releaseDate.substr(0, 4)}
             <h1>
               {movie.title}
             </h1>
-            <div className="movie__overview">
-              <BlockContent blocks={movie.overview} />
+            <div className="overview">
+              <BlockContent
+                blocks={movie.overview}
+                serializers={serializers}
+                dataset={sanity.clientConfig.dataset}
+                projectId={sanity.clientConfig.projectId}
+              />
             </div>
           </div>
           <h2>Cast</h2>
